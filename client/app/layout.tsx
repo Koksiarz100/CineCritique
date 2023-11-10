31cb0af230e400553eb5a254a04622692e7f7b41
Jarosław Zwierz <j20011812@o2.pl>
2023-11-10 18:26:14 +0100

    added base responsibility

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children,}: { children: React.ReactNode }) {
   return (
     <html lang="pl">
-      <body>
+      <body suppressHydrationWarning={true}>
         <Nav />
         <Banner />
         {children}
