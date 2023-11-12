1f85841e646890a6c53e5f1fe267bb47f4a24324
Marta Kolanko <marta.j.kolanko@gmail.com>
2023-11-12 15:04:56 +0100

    (Done) Changed not found page

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -5,11 +5,15 @@ import './styles/notfound.scss'
 export default function NotFound() {
   return (
     <main>
+      <div className='error404'>
+        Hmm... Looks like you shouldn't be here
+      </div>
     <div>
-      <h2 className='notfound'>Not Found</h2>
-      <p className='message'>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <h2 className='notfound'>Page Not Found</h2>
+      <p className='message'>We are so sorry. It seems like we don't have the page you're looking for yet. Keep calm and return to our <b><Link href="/">Home Page</Link></b>.</p>
+      <p className='niceday'>Have a nice day!</p>
     </div>
+    <footer></footer>
     </main>
   )
 }
