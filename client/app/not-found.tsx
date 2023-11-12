a94df3d2c84eb709754b5e1714537a10b1277a04
Marta Kolanko <marta.j.kolanko@gmail.com>
2023-11-12 15:55:29 +0100

    Fixed deployment error

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -6,11 +6,11 @@ export default function NotFound() {
   return (
     <main>
       <div className='error404'>
-        Hmm... Looks like you shouldn't be here
+        Hmm... Looks like you shouldn&apos;t be here
       </div>
     <div>
       <h2 className='notfound'>Page Not Found</h2>
-      <p className='message'>We are so sorry. It seems like we don't have the page you're looking for yet. Keep calm and return to our <b><Link href="/">Home Page</Link></b>.</p>
+      <p className='message'>We are so sorry. It seems like we don&apos;t have the page you&apos;re looking for yet. Keep calm and return to our <b><Link href="/">Home Page</Link></b>.</p>
       <p className='niceday'>Have a nice day!</p>
     </div>
     </main>
