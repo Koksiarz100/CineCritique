bd562395899e143062392ef4af6081af41d8914c
Marta Kolanko <marta.j.kolanko@gmail.com>
2023-11-12 17:49:06 +0100

    Tested sidebar and some progress in profile page

--- client/app/components/app/Profile.tsx
+++ client/app/components/app/Profile.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
+function Sidebar() {
+    return (
+        <>
+        <div className='wrapper'><div className='profile-info'>Informacje</div></div>
+        <div className='container'>
+        </div></>
+
+    )
+  }
+
+  function MainContent(){
+    return (
+    <div className='profile-content'></div>
+    )
+  }
+
 export default function Prof() {
     return (
       <>
         <div className='profile-title'>Profil</div>
+        <div className='mainside'>
+        <Sidebar/>
+        <MainContent/>
+        </div>
       </>
     )
   }
