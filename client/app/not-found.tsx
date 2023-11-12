262e3380eed6d636b999829e30e5e9b6301ab917
Marta Kolanko <marta.j.kolanko@gmail.com>
2023-11-12 13:46:20 +0100

    (In progress) Changing not found page

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link'
- 
+
+import './styles/notfound.scss'
+
 export default function NotFound() {
   return (
+    <main>
     <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
+      <h2 className='notfound'>Not Found</h2>
+      <p className='message'>Could not find requested resource</p>
       <Link href="/">Return Home</Link>
     </div>
+    </main>
   )
 }
