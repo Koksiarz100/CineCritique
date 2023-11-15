f0b4568aff5b228909841c4f5357cdb99f784109
Jarosław Zwierz <j20011812@o2.pl>
2023-11-15 16:09:15 +0100

    added sections in movie page + scss

--- client/app/movie/[id]/page.tsx
+++ client/app/movie/[id]/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 import './styles/move.scss'
 
@@ -14,20 +15,44 @@ export default function page({ params }: { params: { id: string } }) {
         <Link href='/'>Recenzje</Link>
       </div>
       <div className='movie-wrapper'>
-        <div className='movie-info'>
-
+        <div className='movie-main-info'>
+          <div className='movie-image'>
+            <Image src='/placeholder.png' quality={100} alt='test' width={200} height={300} className='card-image'/>
+          </div>
+          <div className='movie-description'>
+            <span className='movie-title-text'>
+              Test
+            </span>
+            <span className='movie-description-text'>
+              Test
+            </span>
+          </div>
         </div>
-        <div className='movie-description'>
-
+        <div className='movie-info'>
+          <div className='movie-info-title'>
+            Informacje
+          </div>
+          <div className='movie-info-main'>
+            <span>Id filmu: {params.id}</span>
+          </div>
         </div>
         <div className='movie-cast'>
-
+          <div className='movie-cast-title'>
+            Obsada
+          </div>
+          <div className='movie-cast-description'>
+            Test
+          </div>
         </div>
         <div className='movie-reviews'>
-
+          <div className='movie-reviews-title'>
+            Recenzje
+          </div>
+          <div className='movie-reviews-description'>
+            Test
+          </div>
         </div>
       </div>
-      <h1>Movie: {params.id}</h1>
     </div>
   )
 }
