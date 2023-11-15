0ad1e58b07361467c29b2fb67ac1786490c1a078
Jarosław Zwierz <j20011812@o2.pl>
2023-11-15 15:36:34 +0100

    added nav in movie page

--- client/app/movie/[id]/page.tsx
+++ client/app/movie/[id]/page.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import Link from 'next/link'
 
 import './styles/move.scss'
 
 export default function page({ params }: { params: { id: string } }) {
   console.log(params)
   return (
-    <div className='movie-wrapper'>
+    <div className='movie-site-wrapper'>
+      <div className='movie-nav'>
+        <Link href='/'>Dane</Link>
+        <Link href='/'>Opis</Link>
+        <Link href='/'>Obsada</Link>
+        <Link href='/'>Recenzje</Link>
+      </div>
+      <div className='movie-wrapper'>
+        <div className='movie-info'>
+
+        </div>
+        <div className='movie-description'>
+
+        </div>
+        <div className='movie-cast'>
+
+        </div>
+        <div className='movie-reviews'>
+
+        </div>
+      </div>
       <h1>Movie: {params.id}</h1>
     </div>
   )
