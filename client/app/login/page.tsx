324e8b275b112a50069c4b4ebed2496846e30a17
Jarosław Zwierz <j20011812@o2.pl>
2023-11-17 22:29:02 +0100

    join together login and register function into one

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -6,15 +6,30 @@ import axios from 'axios'
 
 import './styles/login.scss'
 
-function Login() {
+interface AuthFormProps {
+  formType: 'login' | 'register';
+}
+
+const AuthForm: React.FC<AuthFormProps> = ({ formType }) => {
   const [username, setUsername] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [repeatPassword, setRepeatPassword] = useState<string>('')
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault()
 
     try {
-      const response = await axios.post('http://127.0.0.1:5000/api/login', { username, password })
+      let response
+      if (formType === 'login') {
+        response = await axios.post('http://127.0.0.1:5000/api/login', { username, password })
+        console.log('Sending request');
+      } else if (formType === 'register') {
+        if (password !== repeatPassword) {
+          console.error('Passwords do not match')
+          return
+        }
+        response = await axios.post('http://127.0.0.1:5000/api/register', { username, password })
+      }
       const token = response.data.token
       localStorage.setItem('token', token)
 
@@ -24,45 +39,40 @@ function Login() {
   }
 
   return (
-    <div className='login-form'>
-      <span className='login-form-title'>Zaloguj się</span>
+    <div className={`${formType}-form`}>
+      <span className={`${formType}-form-title`}>{formType === 'login' ? 'Zaloguj się' : 'Zarejestruj się'}</span>
       <form onSubmit={handleSubmit}>
-      <input 
-          type='text' 
-          placeholder='Login' 
-          className='login-form-input' 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} 
-        />
         <input 
-          type='password' 
-          placeholder='Hasło' 
-          className='login-form-input' 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-        />
-        <button type='submit' className='login-form-button'>Zaloguj</button>
-      </form>
-    </div>
-  )
-}
-
-function Register() {
-  return (
-    <div className='register-form'>
-      <span className='register-form-title'>Zarejestruj się</span>
-      <form>
-        <input type='text' placeholder='Login' className='register-form-input' />
-        <input type='password' placeholder='Hasło' className='register-form-input' />
-        <input type='password' placeholder='Powtórz hasło' className='register-form-input' />
-        <button type='submit' className='register-form-button'>Zarejestruj</button>
+            type='text' 
+            placeholder='Login' 
+            className={`${formType}-form-input`} 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+          />
+          <input 
+            type='password' 
+            placeholder='Hasło' 
+            className={`${formType}-form-input`} 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+          {formType === 'register' && (
+            <input 
+              type='password' 
+              placeholder='Powtórz hasło' 
+              className={`${formType}-form-input`} 
+              value={repeatPassword} 
+              onChange={(e) => setRepeatPassword(e.target.value)} 
+            />
+          )}
+          <button type='submit' className={`${formType}-form-button`}>{formType === 'login' ? 'Zaloguj' : 'Zarejestruj'}</button>
       </form>
     </div>
   )
 }
 
 export default function Page() {
-  const [formstate, setFormstate] = useState<'login' | 'register'>('login')
+  const [formState, setFormstate] = useState<'login' | 'register'>('login')
 
   return (
     <div className='login-wrapper' id='login'>
@@ -70,14 +80,14 @@ export default function Page() {
         <Link href='#login' onClick={
           () => setFormstate('login')
         } 
-        className={formstate === 'login' ? 'active' : ''}>Login</Link>
+        className={formState === 'login' ? 'active' : ''}>Login</Link>
         <Link href='#register' onClick={
           () => setFormstate('register')
         }
-        className={formstate === 'register' ? 'active' : ''}>Rejestracja</Link>
+        className={formState === 'register' ? 'active' : ''}>Rejestracja</Link>
       </div>
       <div className='login-main'>
-        {formstate === 'login' ? <Login /> : <Register />}
+        <AuthForm formType={formState} />
       </div>
     </div>
   )
