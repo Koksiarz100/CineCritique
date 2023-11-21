93fd3df631844a2290a38aa92a0be373f3b07826
Jarosław Zwierz <54108886+Koksiarz100@users.noreply.github.com>
2023-11-21 20:21:28 +0100

    Update API.tsx

--- client/app/API.tsx
+++ client/app/API.tsx
@@ -1,3 +1,3 @@
-export const api = 'http://localhost:5000'
+export const api = 'https://cine-critique-site.cyclic.app'
 
-export const images = 'http://localhost:5000/images'
+export const images = 'https://cine-critique-site.cyclic.app'
