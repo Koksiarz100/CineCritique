863f7f8a94bdf4d0b397fa1240104179cebef2e0
Jarosław Zwierz <jaroslaw.zwierz@student.po.edu.pl>
2023-11-27 23:27:47 +0100

    added path for testing on localhost

--- client/app/API.tsx
+++ client/app/API.tsx
@@ -1,6 +1,7 @@
-// Change for production
-// Take links from vercel
+// Testing
+export const api = 'http://localhost:5000'
+export const images = 'http://localhost:5000/images'
 
-export const api = 'https://cine-critique-site.cyclic.app'
-
-export const images = 'https://cine-critique-site.cyclic.app/images'
+// Production
+//export const api = 'https://cine-critique-site.cyclic.app'
+//export const images = 'https://cine-critique-site.cyclic.app/images'
