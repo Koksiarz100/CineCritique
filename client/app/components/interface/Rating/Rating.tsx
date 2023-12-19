4022321319ff76220962dfecfe9df2b9b1c37487
Jarosław Zwierz <j20011812@o2.pl>
2023-12-19 16:56:31 +0100

    added controls, still need to change stars

--- client/app/components/interface/Rating/Rating.tsx
+++ client/app/components/interface/Rating/Rating.tsx
@@ -1,17 +1,41 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 import './rating.scss'
 
 export default function Rating() {
+
   return (
     <div className='stars-wrapper'>
-      Stars
-      <Image src='/stars/starFull.png' alt='rating' width={32} height={32} />
-      <Image src='/stars/starFull.png' alt='rating' width={32} height={32} />
-      <Image src='/stars/starFull.png' alt='rating' width={32} height={32} />
-      <Image src='/stars/starFull.png' alt='rating' width={32} height={32} />
-      <Image src='/stars/starFull.png' alt='rating' width={32} height={32} />
+      <form>
+        <div className='star-main'>
+          <input type='radio' id='one' name='stars' value={10} className='star-button' />
+          <input type='radio' id='oneHalf' name='stars' value={20} className='star-button' />
+          <Image src='/stars/starEmpty.png' alt='rating' className='star-img' width={32} height={32} />
+        </div>
+        <div className='star-main'>
+          <input type='radio' id='two' name='stars' value={30} className='star-button' />
+          <input type='radio' id='twoHalf' name='stars' value={40} className='star-button' />
+          <Image src='/stars/starEmpty.png' alt='rating' className='star-img' width={32} height={32} />
+        </div>
+        <div className='star-main'>
+          <input type='radio' id='three' name='stars' value={50} className='star-button' />
+          <input type='radio' id='threeHalf' name='stars' value={60} className='star-button' />
+          <Image src='/stars/starEmpty.png' alt='rating' className='star-img' width={32} height={32} />
+        </div>
+        <div className='star-main'>
+          <input type='radio' id='four' name='stars' value={70} className='star-button' />
+          <input type='radio' id='fourHalf' name='stars' value={80} className='star-button' />
+          <Image src='/stars/starEmpty.png' alt='rating' className='star-img' width={32} height={32} />
+        </div>
+        <div className='star-main'>
+          <input type='radio' id='five' name='stars' value={90} className='star-button' />
+          <input type='radio' id='fiveHalf' name='stars' value={100} className='star-button' />
+          <Image src='/stars/starEmpty.png' alt='rating' className='star-img' width={32} height={32} />
+        </div>
+      </form>
     </div>
   )
 }
