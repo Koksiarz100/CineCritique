bfcf5d9619887bd4407f42321e6ea41638912fc7
Jarosław Zwierz <jaroslaw.zwierz@student.po.edu.pl>
2024-02-26 18:17:24 +0100

    starting work on dashboard

--- client/app/(pages)/dashboard/page.tsx
+++ client/app/(pages)/dashboard/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
+
+import './styles/dashboard.scss'
 
 
 export default function page() {
   return (
-    <div>page</div>
+    <div className='dashboard-wrapper'>
+      <div className='dashboard-nav'>
+        <Link href='#info'>Dane</Link>
+        <Link href='#description'>Opis</Link>
+        <Link href='#cast'>Obsada</Link>
+        <Link href='#reviews'>Recenzje</Link>
+      </div>
+      Dashboard
+    </div>
   )
 }
